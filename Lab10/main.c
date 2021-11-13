86a219e56e40d68f92a103a65cba82ca91f0b0ea
MarkRibeiro <markpribeiro@hotmail.com>
2021-11-13 07:17:15 -0300

    Refatora main.c

--- Lab10/main.c
+++ Lab10/main.c
@@ -4,144 +4,121 @@
 
 #define TOL 1.0e-7
 #define RELAX 1.1
+
+void printaVetor(double *x, int n){
+    printf("[");
+    for(int i=0; i<n;i++)
+    {
+        if(i == n-1) {
+            printf("%g",x[i]);
+        }
+        else {
+            printf("%g, ",x[i]);
+        }
+    }
+    printf("]\n");
+}
+
+void zeraVetor(double *x, int n){
+    for(int i=0;i<n;i++)
+    {
+        x[i]=0;
+    }
+}
+
+void resultados(char f, int t, int n, double **A, double *b, double *x){
+    int iters;
+    if(f == 'g'){
+        iters = gauss_seidel(n,A,b,x,TOL);
+        printf("\n--GaussSeidel--");
+    }
+    if(f == 's'){
+        iters = SOR(n,A,b,x,TOL, RELAX);
+        printf("\n--SOR--");
+    }
+
+    if(t < 3){
+        printf("Problema %d--\n", t);
+    }
+    else {
+        printf("Linhas Trocadas--\n");
+    }
+    
+    printf("iteracoes: %d\n",iters);
+    printf("solucao encontrada: ");
+    printaVetor(x, n);
+    zeraVetor(x, n);
+}
+
+void teste1(){
+    int n = 2;
+    int t = 1;
+    double x[2] = {0,0};
+    double b[2] = {5,5};
+    double A0[2] = {3,1};
+    double A1[2] = {1,2};
+    double **A = malloc(2*sizeof(double*));
+    A[0]=A0;
+    A[1]=A1;
+
+    resultados('g', t, n, A, b, x);
+    resultados('s', t, n, A, b, x);
+    free(A);
+}
+
+void teste2(){
+    int n = 6;
+    int t = 2;
+    double x[6] = {0,0,0,0,0,0};
+    double b[6] = {2.5,1.5,1,1,1.5,2.5};
+    double A0[6] = {3,-1,0,0,0,0.5};
+    double A1[6] = {-1,3,-1,0,0.5,0};
+    double A2[6] = {0,-1,3,-1,0,0};
+    double A3[6] = {0,0,-1,3,-1,0};
+    double A4[6] = {0,0.5,0,-1,3,-1};
+    double A5[6] = {0.5,0,0,0,-1,3};
+    double **A = malloc(6*sizeof(double*));
+    A[0]=A0;
+    A[1]=A1;
+    A[2]=A2;
+    A[3]=A3;
+    A[4]=A4;
+    A[5]=A5;
+
+    resultados('g', t, n, A, b, x);
+    resultados('s', t, n, A, b, x);
+    free(A);
+}
+
+void teste3(){
+    int n = 6;
+    int t = 3;
+    double x[6] = {0,0,0,0,0,0};
+    double b[6] = {2.5,1.5,1,1,1.5,2.5};
+    double A0[6] = {3,-1,0,0,0,0.5};
+    double A1[6] = {-1,3,-1,0,0.5,0};
+    double A2[6] = {0,-1,3,-1,0,0};
+    double A3[6] = {0,0,-1,3,-1,0};
+    double A4[6] = {0,0.5,0,-1,3,-1};
+    double A5[6] = {0.5,0,0,0,-1,3};
+    double **A = malloc(6*sizeof(double*));
+    A[0]=A5;
+    A[1]=A1;
+    A[2]=A2;
+    A[3]=A3;
+    A[4]=A4;
+    A[5]=A0;
+
+    resultados('g', t, n, A, b, x);
+    resultados('s', t, n, A, b, x);
+    free(A);
+}
+
 int main()
 {
-    do
-    {   
-        double A0[2] = {3,1};
-        double A1[2] = {1,2};
-        double **A = malloc(2*sizeof(double*));
-        A[0]=A0;
-        A[1]=A1;
-        
-        double x[2] = {0,0};
-        double b[2] = {5,5};
-
-        printf("\n--GaussSeidel--Problema 1--\n");
-        int iters = gauss_seidel(2,A,b,x,TOL);
-        printf("iteracoes: %d\n",iters);
-        printf("solucao encontrada: [%g , %g] \n",x[0],x[1]);
-
-        for(int i=0;i<2;i++)
-        {
-            x[i]=0;
-        }
-
-        printf("\n--SOR--Problema 1--\n");
-        iters = SOR(2,A,b,x,TOL,RELAX);
-        printf("iteracoes: %d\n",iters);
-        printf("solucao encontrada: [%g , %g] \n",x[0],x[1]);
-
-        free(A);
-    }while(0);
-
-    do
-    {   
-        double A0[6] = {3,-1,0,0,0,0.5};
-        double A1[6] = {-1,3,-1,0,0.5,0};
-        double A2[6] = {0,-1,3,-1,0,0};
-        double A3[6] = {0,0,-1,3,-1,0};
-        double A4[6] = {0,0.5,0,-1,3,-1};
-        double A5[6] = {0.5,0,0,0,-1,3};
-        double **A = malloc(6*sizeof(double*));
-        A[0]=A0;
-        A[1]=A1;
-        A[2]=A2;
-        A[3]=A3;
-        A[4]=A4;
-        A[5]=A5;
-        
-        double x[6] = {0,0,0,0,0,0};
-        double b[6] = {2.5,1.5,1,1,1.5,2.5};
-
-        printf("\n--GaussSeidel--Problema 2--\n");
-        int iters = gauss_seidel(6,A,b,x,TOL);
-        printf("iteracoes: %d\n",iters);
-        printf("solucao encontrada: \n");
-        printf("[");
-        for(int i=0; i<6;i++)
-        {
-            printf("%g, ",x[i]);
-        }
-        printf("]\n");
-
-        for(int i=0;i<6;i++)
-        {
-            x[i]=0;
-        }
-
-        printf("\n--SOR--Problema 2--\n");
-        iters = SOR(6,A,b,x,TOL,RELAX);
-        printf("iteracoes: %d\n",iters);
-        printf("solucao encontrada: \n");
-        printf("[");
-        for(int i=0; i<6;i++)
-        {
-            printf("%g, ",x[i]);
-        }
-        printf("]\n");
-
-        for(int i=0;i<6;i++)
-        {
-            x[i]=0;
-        }
-
-        free(A);
-    }while(0);
-
-    do
-    {   
-        double A0[6] = {3,-1,0,0,0,0.5};
-        double A1[6] = {-1,3,-1,0,0.5,0};
-        double A2[6] = {0,-1,3,-1,0,0};
-        double A3[6] = {0,0,-1,3,-1,0};
-        double A4[6] = {0,0.5,0,-1,3,-1};
-        double A5[6] = {0.5,0,0,0,-1,3};
-        double **A = malloc(6*sizeof(double*));
-        A[0]=A5;
-        A[1]=A1;
-        A[2]=A2;
-        A[3]=A3;
-        A[4]=A4;
-        A[5]=A0;
-        
-        double x[6] = {0,0,0,0,0,0};
-        double b[6] = {2.5,1.5,1,1,1.5,2.5};
-
-        printf("\n--GaussSeidel--Linhas Trocadas--\n");
-        int iters = gauss_seidel(6,A,b,x,TOL);
-        printf("iteracoes: %d\n",iters);
-        printf("solucao encontrada: \n");
-        printf("[");
-        for(int i=0; i<6;i++)
-        {
-            printf("%g, ",x[i]);
-        }
-        printf("]\n");
-
-        for(int i=0;i<6;i++)
-        {
-            x[i]=0;
-        }
-
-        printf("\n--SOR--Linhas Trocadas--\n");
-        iters = SOR(6,A,b,x,TOL,RELAX);
-        printf("iteracoes: %d\n",iters);
-        printf("solucao encontrada: \n");
-        printf("[");
-        for(int i=0; i<6;i++)
-        {
-            printf("%g, ",x[i]);
-        }
-        printf("]\n");
-
-        for(int i=0;i<6;i++)
-        {
-            x[i]=0;
-        }
-
-        free(A);
-    }while(0);
+    teste1();
+    teste2();
+    teste3();
 
     return 0;
 }
