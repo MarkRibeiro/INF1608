da648748c2bf862a8a326da059f45a55a12f8816
MarkRibeiro <markpribeiro@hotmail.com>
2021-12-05 15:17:16 -0300

    Cria projeto final

---
+++ pf/ode.h
@@ -0,0 +1,4 @@
+//Aluno: Mark Ribeiro
+//Matrícula: 1612043
+double RungeKutta(double t0, double t1, double h, double y0, double (*f) (double t, double y));
+double RungeKuttaAcoplado(double t0, double t1, double y0, double (*f) (double t, double y), double tol);
